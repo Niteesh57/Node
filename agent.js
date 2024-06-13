27c90d78ca7988c7be1f123453db4f1817f43431
Niteesh.bv <niteesh.bv@gmail.com>
2024-06-13 07:34:50 +0530

    Initial commit 15

---
+++ agent.js
@@ -0,0 +1,15 @@
+// var http = require('http');
+
+// http.createServer((req,res)=>{
+//     res.writeHead(200,{'Content-Type':'text/plain'});
+//     res.end('Hello World\n');
+// }).listen(3000);
+
+// const { kMaxLength } = require('buffer');
+// const { create } = require('domain');
+// var http = require('http')
+
+// age = http.Agent({keepAlive:true})
+// creates = age.createConnection;
+// console.log('conected',creates);
+
