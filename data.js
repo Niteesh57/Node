b9954508787e5e818faf53d565a4a865323f3572
Niteesh.bv <niteesh.bv@gmail.com>
2024-07-21 21:20:58 +0530

    Initial commit 47

--- data.js
+++ data.js
@@ -1,9 +1,6 @@
 var buf = require('buffer');
 
-// Create and write a buffer of 10 bytes
 var buf1 = new Buffer(10);
-//buf1.fill(0x00);
-buf1.fill(0x00);
 
 console.log('Buffer 1 contents: ' + buf1.toString('hex'));
 t = "Hello World"
@@ -12,6 +9,3 @@ b = new Buffer.from(t,'utf8')
 console.log('Buffer 2:' + b.toString('hex'))
 
 var buf2 = Buffer.alloc(900);
-
-
-// Concatenate two buffers
